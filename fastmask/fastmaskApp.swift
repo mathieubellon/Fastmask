596da3517796ce11f5cfe2171466e564c7268e81
Mathieu Bellon <1445897+mathieubellon@users.noreply.github.com>
2022-08-14 11:48:17 +0200

    Initial Commit

---
+++ fastmask/fastmaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastmaskApp.swift
+//  fastmask
+//
+//  Created by Mathieu Bellon on 14/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct fastmaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
